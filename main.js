3e68e4f7e9199fbe714e2255e0bf3b646262bace
jpswt <joel_swt@yahoo.com>
2022-03-10 23:25:22 -0600

    first commit of all functions working

--- main.js
+++ main.js
@@ -9,59 +9,134 @@ console.log(new Date)
 
 const displayDate = () => {
   const currentDate = new Date()
-
-  document.getElementById("display-element").innerHTML = currentDate;
 }
- 
+
 
 // Write a JavaScript program to convert a number to a string.
 
+//Declaring the function numberToString with the input parameter of inputNumber
+const numberToString = (inputNumber) => {
+  //Declare a varible that equals the result of inputNumber to a string
+  let convertNumToString = inputNumber.toString();
+  //Logging the type of convertNumToString which equals a string
+  console.log(convertNumToString, " has been converted to " + typeof (convertNumToString));
+}
+//Call the function numberToString with input of 8
+numberToString(8)
 
 
 // Write a JavaScript program to convert a string to the number.
 
+//Declaring the function stringToNumber with the input parameter of inputString
+const stringToNumber = (inputString) => {
+  //Declare a variable of that equals the parseInt of the inputString
+  let convertedNumber = parseInt(inputString);
+  //Logging the type of convertedNumber which equals a number
+  console.log(convertedNumber, " has been converted to " + typeof (convertedNumber));
+}
+//call the function stringToNumber with input of 5
+stringToNumber(5)
 
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
-  // * Boolean
-  // * Null
-  // * Undefined
-  // * Number
-  // * NaN
-  // * String
-  
+// * Boolean
+// * Null
+// * Undefined
+// * Number
+// * NaN
+// * String
 
-  
+// Declaring a function getInputType with a parameter of input 
+const getInputType = (input) => {
+  //Declare a variable of inputType which equals the typeof input
+  let inputType = typeof input
+  //Logging the type of input type which equals the type of given input
+  console.log('The datetype is ' + inputType)
+
+}
+
+//Multiple calls of the getInputType function to display various input types
+getInputType(true);
+getInputType(null)
+getInputType(NaN)
+getInputType(undefined)
+getInputType(12)
+getInputType("12")
+getInputType(getInputType)
+
+
 // Write a JavaScript program that adds 2 numbers together.
 
+//Declaring a function addTwoNumbers with parameters of num1 and num2
+const addTwoNumbers = (num1, num2) => {
+  //Decalare a variable of sumOfNumbers that is the summation of num1 + num2
+  let sumOfNumbers = num1 + num2
+  //Logging to the summation of num1 + num2 with variable sumOfNumbers
+  console.log(`The sum of ${num1} and ${num2} is ${sumOfNumbers}.`)
 
+}
+//Invoking the function addTwoNumbers with 2 and 8.  Summation should equal 10
+addTwoNumbers(2, 8)
 
 // Write a JavaScript program that runs only when 2 things are true.
 
-
+//Decalring a function of bothEvenNumbers to test if both parameters (num1 AND num2) are even numbers
+const bothEvenNumbers = (num1, num2) => {
+  //Determine if both num1 and num2 are even with modulus operand equal to zero
+  if ((num1 % 2 === 0) && (num2 % 2 === 0)) {
+    // Logging message that passes if both numbers are even
+    console.log(`Both ${num1} and ${num2} are even numbers.`)
+  } else {
+    // Logging message if both numbers are not even
+    console.log(`Both ${num1} and ${num2} are not even numbers, please try again.`)
+  }
+}
+// Invoking the function bothEvenNumbers to with input of 10 an 20 (both even numbers).  Outcome should test true 
+bothEvenNumbers(10, 20)
+// Invoking the function bothEvenNumbers to with input of 10 an 11 (one even number).  Outcome should test false 
+bothEvenNumbers(10, 11)
+// Invoking the function bothEvenNumbers to with input of 13 an 11 (zero even number).  Outcome should test false 
+bothEvenNumbers(13, 11)
 
 // Write a JavaScript program that runs when 1 of 2 things are true.
 
+//Declaring a function of oneEvenNumber to test if one of the parameters (num1 OR num2) is an even number
+const oneEvenNumber = (num1, num2) => {
+  //Determine if either num1 or num2 is even with modulus operand equal to zero
+  if ((num1 % 2 === 0) || (num2 % 2 === 0)) {
+    // Logging message passes if either num1 OR num2 is an even number
+    console.log(`Congrats. Atleast one of the numbers: ${num1} or ${num2}, is an even number.`)
+  } else {
+    // Logging message passes if neither num1 OR num2 is an even number
+    console.log(`Neither ${num1} or ${num2} is an even number, please try again.`)
+  }
+}
+
+// Invoking the function oneEvenNumber to with input of 10 an 20 (both even numbers).  Outcome should test true 
+oneEvenNumber(10, 20)
+// Invoking the function oneEvenNumber to with input of 10 an 11 (one even number).  Outcome should test true 
+oneEvenNumber(10, 11)
+// Invoking the function oneEvenNumber to with input of 13 an 11 (zero even number).  Outcome should test false 
+oneEvenNumber(13, 11)
 
 
 // Write a JavaScript program that runs when both things are not true.  
 
-// ***************************
-//         PART TWO
-// ***************************
+//Declaring a function of zeroEvenNumbers to test if one of the parameters (num1 OR num2) is an even number
+const zeroEvenNumbers = (num1, num2) => {
+  //Determine if neither num1 or num2 is even with modulus operand not equal to zero
+  if ((num1 % 2 !== 0 && num2 % 2 !== 0)) {
+    // Logging passing message if both num1 and num2 are not even numbers
+    console.log(`Congrats, niether ${num1} or ${num2} is even.`)
+  } else {
+    // Logging passing message if either num1 or num2 are not even numbers
+    console.log(`Either ${num1} or ${num2} is even, please try again.`)
+  }
+}
 
-// 1. download Live-Server by Ritwick Dey, 
-// 2. reload VS Code, 
-// 3. click the "Go Live" button
-// 4. Go local host 5500 or http://127.0.0.1:5500/index.html to see your web page
-// 5. Or go use the `npm start` command and navigate to localhost:8080 (ctrl + C to close)
-// 6. go to `index.html` 
-// 7. create inputs, buttons and event listeners that render the code blocks you built above to the DOM.
-
-
-
-
-// Additional Resources
-// Video1: https://player.vimeo.com/video/377147232
-// Video2: https://www.youtube.com/embed/bkvH28PXLWc
-// Video3: https://www.youtube.com/embed/TrGI9Yki-24
+// Invoking the function zeroEvenNumbers to with input of 10 an 20 (both even numbers).  Outcome should test false 
+zeroEvenNumbers(10, 20)
+// Invoking the function zeroEvenNumbers to with input of 10 an 11 (one even number).  Outcome should test false 
+zeroEvenNumbers(10, 11)
+// Invoking the function zeroEvenNumbers to with input of 13 an 11 (zero even number).  Outcome should test true 
+zeroEvenNumbers(13, 11)
